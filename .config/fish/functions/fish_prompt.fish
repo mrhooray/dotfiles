3b06f3ea8c352c75558721cc6f42d4ba38a5a112
Rui Hu <code@mrhooray.com>
2018-01-09 01:31:14 -0800

    update fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -237,8 +237,16 @@ function prompt_status -d "the symbols for a non zero exit status, root and back
     end
 end
 
-function type -q -a name -d "Check if a function or program is type -q."
-  type "$name" ^/dev/null >&2
+if printf '%s\n' '2.2.0' $FISH_VERSION | sort --check=silent --version-sort
+  # Current version ≥ 2.2.0
+  function __exists -a name -d "Check if a function or program does exist."
+    command -v "$name" ^/dev/null >&2
+  end
+else
+  # Current version < 2.2.0
+  function __exists -a name -d "Check if a function or program does exist."
+    type "$name" ^/dev/null >&2
+  end
 end
 
 
@@ -251,8 +259,8 @@ function fish_prompt
   prompt_virtual_env
   prompt_user
   prompt_dir
-  type -q hg;  and prompt_hg
-  type -q git; and prompt_git
-  type -q svn; and prompt_svn
+  __exists hg;  and prompt_hg
+  __exists git; and prompt_git
+  __exists svn; and prompt_svn
   prompt_finish
 end
