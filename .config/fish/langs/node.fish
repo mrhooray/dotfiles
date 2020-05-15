fb41521306a8436364e091fba3cc4f84a4b2b8c9
Rui Hu <code@mrhooray.com>
2020-05-15 02:31:38 -0700

    add node lts upgrade function

--- .config/fish/langs/node.fish
+++ .config/fish/langs/node.fish
@@ -4,6 +4,17 @@ function nvm
   bass source $NVM_DIR/nvm.sh --no-use ';' nvm $argv
 end
 
+function nvm_lts
+  set -l next (nvm version-remote --lts)
+  set -l curr (nvm version (string split . $next)[1])
+  if test $curr != $next
+    nvm install --lts --latest-npm
+    nvm reinstall-packages $curr
+    nvm uninstall $curr
+    nvm cache clear
+  end
+end
+
 function snvm
   bass source $NVM_DIR//nvm.sh
 end
