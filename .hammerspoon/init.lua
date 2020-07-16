efe95b4f6dae850c7d45af6e298ec39bb5aecd2b
Rui Hu <code@mrhooray.com>
2020-07-15 22:23:45 -0700

    hammerspoon - next window shortcut

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,7 +7,7 @@ end)
 ----------------------------------------------------
 -- remap arrow keys
 ----------------------------------------------------
-local function pressFn(mods, key)
+local function press(mods, key)
   if key == nil then
     key = mods
     mods = {}
@@ -16,49 +16,53 @@ local function pressFn(mods, key)
   return function() hs.eventtap.keyStroke(mods, key, 1000) end
 end
 
-local function remap(mods, key, pressFn)
-  hs.hotkey.bind(mods, key, pressFn, nil, pressFn)
+local function remap(mods, key, press)
+  hs.hotkey.bind(mods, key, press, nil, press)
 end
 
-remap({'ctrl'}, 'h', pressFn('left'))
-remap({'ctrl'}, 'j', pressFn('down'))
-remap({'ctrl'}, 'k', pressFn('up'))
-remap({'ctrl'}, 'l', pressFn('right'))
+remap({'ctrl'}, 'h', press('left'))
+remap({'ctrl'}, 'j', press('down'))
+remap({'ctrl'}, 'k', press('up'))
+remap({'ctrl'}, 'l', press('right'))
 
-remap({'ctrl', 'shift'}, 'h', pressFn({'shift'}, 'left'))
-remap({'ctrl', 'shift'}, 'j', pressFn({'shift'}, 'down'))
-remap({'ctrl', 'shift'}, 'k', pressFn({'shift'}, 'up'))
-remap({'ctrl', 'shift'}, 'l', pressFn({'shift'}, 'right'))
+remap({'ctrl', 'shift'}, 'h', press({'shift'}, 'left'))
+remap({'ctrl', 'shift'}, 'j', press({'shift'}, 'down'))
+remap({'ctrl', 'shift'}, 'k', press({'shift'}, 'up'))
+remap({'ctrl', 'shift'}, 'l', press({'shift'}, 'right'))
 
-remap({'ctrl', 'cmd'}, 'h', pressFn({'cmd'}, 'left'))
-remap({'ctrl', 'cmd'}, 'j', pressFn({'cmd'}, 'down'))
-remap({'ctrl', 'cmd'}, 'k', pressFn({'cmd'}, 'up'))
-remap({'ctrl', 'cmd'}, 'l', pressFn({'cmd'}, 'right'))
+remap({'ctrl', 'cmd'}, 'h', press({'cmd'}, 'left'))
+remap({'ctrl', 'cmd'}, 'j', press({'cmd'}, 'down'))
+remap({'ctrl', 'cmd'}, 'k', press({'cmd'}, 'up'))
+remap({'ctrl', 'cmd'}, 'l', press({'cmd'}, 'right'))
 
-remap({'ctrl', 'alt'}, 'h', pressFn({'alt'}, 'left'))
-remap({'ctrl', 'alt'}, 'j', pressFn({'alt'}, 'down'))
-remap({'ctrl', 'alt'}, 'k', pressFn({'alt'}, 'up'))
-remap({'ctrl', 'alt'}, 'l', pressFn({'alt'}, 'right'))
+remap({'ctrl', 'alt'}, 'h', press({'alt'}, 'left'))
+remap({'ctrl', 'alt'}, 'j', press({'alt'}, 'down'))
+remap({'ctrl', 'alt'}, 'k', press({'alt'}, 'up'))
+remap({'ctrl', 'alt'}, 'l', press({'alt'}, 'right'))
 
-remap({'ctrl', 'shift', 'cmd'}, 'h', pressFn({'shift', 'cmd'}, 'left'))
-remap({'ctrl', 'shift', 'cmd'}, 'j', pressFn({'shift', 'cmd'}, 'down'))
-remap({'ctrl', 'shift', 'cmd'}, 'k', pressFn({'shift', 'cmd'}, 'up'))
-remap({'ctrl', 'shift', 'cmd'}, 'l', pressFn({'shift', 'cmd'}, 'right'))
+remap({'ctrl', 'shift', 'cmd'}, 'h', press({'shift', 'cmd'}, 'left'))
+remap({'ctrl', 'shift', 'cmd'}, 'j', press({'shift', 'cmd'}, 'down'))
+remap({'ctrl', 'shift', 'cmd'}, 'k', press({'shift', 'cmd'}, 'up'))
+remap({'ctrl', 'shift', 'cmd'}, 'l', press({'shift', 'cmd'}, 'right'))
 
-remap({'ctrl', 'shift', 'alt'}, 'h', pressFn({'shift', 'alt'}, 'left'))
-remap({'ctrl', 'shift', 'alt'}, 'j', pressFn({'shift', 'alt'}, 'down'))
-remap({'ctrl', 'shift', 'alt'}, 'k', pressFn({'shift', 'alt'}, 'up'))
-remap({'ctrl', 'shift', 'alt'}, 'l', pressFn({'shift', 'alt'}, 'right'))
+remap({'ctrl', 'shift', 'alt'}, 'h', press({'shift', 'alt'}, 'left'))
+remap({'ctrl', 'shift', 'alt'}, 'j', press({'shift', 'alt'}, 'down'))
+remap({'ctrl', 'shift', 'alt'}, 'k', press({'shift', 'alt'}, 'up'))
+remap({'ctrl', 'shift', 'alt'}, 'l', press({'shift', 'alt'}, 'right'))
 
-remap({'ctrl', 'cmd', 'alt'}, 'h', pressFn({'cmd', 'alt'}, 'left'))
-remap({'ctrl', 'cmd', 'alt'}, 'j', pressFn({'cmd', 'alt'}, 'down'))
-remap({'ctrl', 'cmd', 'alt'}, 'k', pressFn({'cmd', 'alt'}, 'up'))
-remap({'ctrl', 'cmd', 'alt'}, 'l', pressFn({'cmd', 'alt'}, 'right'))
+remap({'ctrl', 'cmd', 'alt'}, 'h', press({'cmd', 'alt'}, 'left'))
+remap({'ctrl', 'cmd', 'alt'}, 'j', press({'cmd', 'alt'}, 'down'))
+remap({'ctrl', 'cmd', 'alt'}, 'k', press({'cmd', 'alt'}, 'up'))
+remap({'ctrl', 'cmd', 'alt'}, 'l', press({'cmd', 'alt'}, 'right'))
 
-remap({'ctrl', 'cmd', 'alt', 'shift'}, 'h', pressFn({'cmd', 'alt', 'shift'}, 'left'))
-remap({'ctrl', 'cmd', 'alt', 'shift'}, 'j', pressFn({'cmd', 'alt', 'shift'}, 'down'))
-remap({'ctrl', 'cmd', 'alt', 'shift'}, 'k', pressFn({'cmd', 'alt', 'shift'}, 'up'))
-remap({'ctrl', 'cmd', 'alt', 'shift'}, 'l', pressFn({'cmd', 'alt', 'shift'}, 'right'))
+remap({'ctrl', 'cmd', 'alt', 'shift'}, 'h', press({'cmd', 'alt', 'shift'}, 'left'))
+remap({'ctrl', 'cmd', 'alt', 'shift'}, 'j', press({'cmd', 'alt', 'shift'}, 'down'))
+remap({'ctrl', 'cmd', 'alt', 'shift'}, 'k', press({'cmd', 'alt', 'shift'}, 'up'))
+remap({'ctrl', 'cmd', 'alt', 'shift'}, 'l', press({'cmd', 'alt', 'shift'}, 'right'))
+----------------------------------------------------
+-- focus on next window
+----------------------------------------------------
+remap({'cmd'}, 'escape', press({'cmd'}, '`'))
 ----------------------------------------------------
 -- swap backspace and backslash
 ----------------------------------------------------
