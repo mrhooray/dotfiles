b7cb56d65bbaa553c9d521b0345415730e64398c
Rui Hu <code@mrhooray.com>
2021-06-11 00:23:53 -0700

    lang - use fnm instead of nvm for node

--- .config/fish/langs/node.fish
+++ .config/fish/langs/node.fish
@@ -1,20 +1,3 @@
-set -x NVM_DIR ~/.nvm
-
-function nvm
-  bass source $NVM_DIR/nvm.sh --no-use ';' nvm $argv
-end
-
-function nvm_lts
-  set -l next (nvm version-remote --lts)
-  set -l curr (nvm version (string split . $next)[1])
-  if test $curr != $next
-    nvm install --lts --latest-npm
-    nvm reinstall-packages $curr
-    nvm uninstall $curr
-    nvm cache clear
-  end
-end
-
-function snvm
-  bass source $NVM_DIR//nvm.sh
+if type -q fnm
+  fnm env | source
 end
