2da513d093c07d8b2aca22bddb53e542c83b39e6
Rui Hu <code@mrhooray.com>
2023-11-23 19:09:48 -0800

    python - alias

--- config/fish/langs/python.fish
+++ config/fish/langs/python.fish
@@ -2,3 +2,6 @@ set -gx PATH $HOME/.local/bin $PATH
 set -gx PIPENV_VENV_IN_PROJECT 1
 set -gx IPYTHONDIR $XDG_CONFIG_HOME/ipython
 set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
+
+alias pip pip3
+alias python python3
