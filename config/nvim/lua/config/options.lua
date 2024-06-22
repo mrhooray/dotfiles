4f7b16546a42f6260093c2f935254a710153496c
Rui Hu <code@mrhooray.com>
2024-06-22 15:56:05 -0700

    nvim - parity

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,5 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.backup = false
-vim.opt.writebackup = false
 vim.opt.colorcolumn = "120"
+vim.opt.tabstop = 2
+vim.opt.undofile = false
+vim.opt.updatetime = 320
+vim.opt.writebackup = false
+vim.opt.iskeyword:append("-")
+vim.opt.expandtab = true
