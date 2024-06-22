1b2e4037b33bced892fd808cc96d6f3916044b26
Rui Hu <code@mrhooray.com>
2024-06-21 19:03:25 -0700

    fish,git - theme

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,28 +1,29 @@
-set -U fish_color_autosuggestion 9e9e9e
-set -U fish_color_command 00d700
-set -U fish_color_comment 878787
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_end 969696
-set -U fish_color_error ff0000
-set -U fish_color_escape cyan
-set -U fish_color_history_current cyan
-set -U fish_color_host -o cyan
-set -U fish_color_match cyan
-set -U fish_color_normal normal
-set -U fish_color_operator cyan
-set -U fish_color_param dadada
-set -U fish_color_quote ffff87
-set -U fish_color_redirection dadada
-set -U fish_color_search_match --background=purple
-set -U fish_color_selection --background=purple
-set -U fish_color_status red
-set -U fish_color_user -o green
-set -U fish_color_valid_path --underline
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description 555 yellow
-set -U fish_pager_color_prefix cyan
-set -U fish_pager_color_progress cyan
+set -U fish_color_normal cad3f5
+set -U fish_color_command 8aadf4
+set -U fish_color_param f0c6c6
+set -U fish_color_keyword ed8796
+set -U fish_color_quote a6da95
+set -U fish_color_redirection f5bde6
+set -U fish_color_end f5a97f
+set -U fish_color_comment 8087a2
+set -U fish_color_error ed8796
+set -U fish_color_gray 6e738d
+set -U fish_color_selection --background=363a4f
+set -U fish_color_search_match --background=363a4f
+set -U fish_color_option a6da95
+set -U fish_color_operator f5bde6
+set -U fish_color_escape ee99a0
+set -U fish_color_autosuggestion 6e738d
+set -U fish_color_cancel ed8796
+set -U fish_color_cwd eed49f
+set -U fish_color_user 8bd5ca
+set -U fish_color_host 8aadf4
+set -U fish_color_host_remote a6da95
+set -U fish_color_status ed8796
+set -U fish_pager_color_progress 6e738d
+set -U fish_pager_color_prefix f5bde6
+set -U fish_pager_color_completion cad3f5
+set -U fish_pager_color_description 6e738d
 
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
@@ -37,7 +38,7 @@ set -gx RIPGREP_CONFIG_PATH $XDG_CONFIG_HOME/ripgrep/ripgreprc
 set -gx FZF_DEFAULT_COMMAND 'rg --files-with-matches "."'
 set -gx FZF_DEFAULT_OPTS '-m --height 50% --border'
 set -gx NNN_COLORS '#a744b322'
-set -gx NNN_FCOLORS 'c1e2ff6ff36dccf7c6e3aba1'
+set -gx NNN_FCOLORS c1e2ff6ff36dccf7c6e3aba1
 
 alias e $EDITOR
 alias l 'll -A'
@@ -45,24 +46,24 @@ alias g 'lazygit -ucd $HOME/.config/lazygit/'
 alias nnn 'nnn -dHo'
 
 function source_by_unix
-  switch (uname)
-    case Linux
-      source (string replace -r '.fish$' '\-linux.fish' (status -f))
-    case Darwin
-      source (string replace -r '.fish$' '\-darwin.fish' (status -f))
-  end
+    switch (uname)
+        case Linux
+            source (string replace -r '.fish$' '\-linux.fish' (status -f))
+        case Darwin
+            source (string replace -r '.fish$' '\-darwin.fish' (status -f))
+    end
 end
 
 source_by_unix
 
 if test -e $HOME/.local.fish
-  source $HOME/.local.fish
+    source $HOME/.local.fish
 end
 
 for f in (dirname (status -f))/langs/*.fish
-  source $f
+    source $f
 end
 
 if type -q starship
-  starship init fish | source
+    starship init fish | source
 end
