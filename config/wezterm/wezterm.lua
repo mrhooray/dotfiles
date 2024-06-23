ea206ae1e3ecfd46d3253ee50d81b4fff78b5df2
Rui Hu <code@mrhooray.com>
2024-06-22 22:53:49 -0700

    nvim,wezterm - ui tweaks

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,14 +1,41 @@
 local wezterm = require("wezterm")
+local p = require("palette")
 local act = wezterm.action
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = "catppuccin-macchiato"
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 512
 config.initial_rows = 128
 config.scrollback_lines = 65536
 config.tab_max_width = 32
 config.use_fancy_tab_bar = false
+
+config.colors = {
+  tab_bar = {
+    background = p.base,
+    active_tab = {
+      fg_color = p.text,
+      bg_color = p.surface2,
+    },
+    inactive_tab = {
+      fg_color = p.subtext0,
+      bg_color = p.base,
+    },
+    inactive_tab_hover = {
+      fg_color = p.text,
+      bg_color = p.surface0,
+    },
+    new_tab = {
+      fg_color = p.subtext0,
+      bg_color = p.base,
+    },
+    new_tab_hover = {
+      fg_color = p.text,
+      bg_color = p.surface0,
+    },
+  },
+}
 
 config.keys = {
   { mods = "CMD", key = "Enter", action = act.ActivateCopyMode },
