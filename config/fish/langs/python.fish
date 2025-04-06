e4e63990617592564e6377928e572b811a91777d
Rui Hu <code@mrhooray.com>
2025-04-06 13:53:57 -0700

    lang - python - remove rye

--- config/fish/langs/python.fish
+++ config/fish/langs/python.fish
@@ -2,4 +2,3 @@ set -gx PIPENV_VENV_IN_PROJECT 1
 set -gx IPYTHONDIR $XDG_CONFIG_HOME/ipython
 set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
 set -gx PYTHON_HISTORY $XDG_STATE_HOME/python/python_history
-fish_add_path $HOME/.rye/shims
