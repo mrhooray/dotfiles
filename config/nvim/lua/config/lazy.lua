b2670b777d055dc9bdef935131e91ecd7f5debee
Rui Hu <code@mrhooray.com>
2025-06-08 10:37:46 -0700

    nvim - biome over prettier

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.biome" },
     -- import/override with your plugins
     { import = "plugins" },
   },
