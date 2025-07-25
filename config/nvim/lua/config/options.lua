3d81abe9a92a991ec58fb5df9fc5a94f4ba9dbf3
Rui Hu <code@mrhooray.com>
2025-07-24 17:14:59 -0700

    nvim - tabstop

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -7,7 +7,7 @@ vim.opt.expandtab = true
 vim.opt.relativenumber = false
 vim.opt.shiftwidth = 0
 vim.opt.swapfile = false
-vim.opt.tabstop = 2
+vim.opt.tabstop = 4
 vim.opt.undofile = false
 vim.opt.writebackup = false
 
