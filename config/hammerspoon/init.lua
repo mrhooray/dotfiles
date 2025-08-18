e1aa1dd4a616a3932646c560354839678f4090e8
Rui Hu <code@mrhooray.com>
2025-08-18 09:09:47 -0700

    hammerspoon - remove low battery alert

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,25 +1,27 @@
 ----------------------------------------------------
 -- utilities
 ----------------------------------------------------
-local logger = hs.logger.new('dotfiles','info')
+local logger = hs.logger.new("dotfiles", "info")
 
 local function press(mods, key)
-  if key == nil then
-    key = mods
-    mods = {}
-  end
+	if key == nil then
+		key = mods
+		mods = {}
+	end
 
-  return function() hs.eventtap.keyStroke(mods, key, 1000) end
+	return function()
+		hs.eventtap.keyStroke(mods, key, 1000)
+	end
 end
 
 local function remap(mods, key, press)
-  hs.hotkey.bind(mods, key, press, nil, press)
+	hs.hotkey.bind(mods, key, press, nil, press)
 end
 ----------------------------------------------------
 -- reload
 ----------------------------------------------------
-hs.hotkey.bind({"cmd", "ctrl"}, "r", function()
-  hs.reload()
+hs.hotkey.bind({ "cmd", "ctrl" }, "r", function()
+	hs.reload()
 end)
 ----------------------------------------------------
 -- keymap for arrows
@@ -31,41 +33,24 @@ remap({'alt'}, 'l', press('right'))
 ----------------------------------------------------
 -- focus on next window
 ----------------------------------------------------
-remap({'cmd'}, 'escape', press({'cmd'}, '`'))
-----------------------------------------------------
--- low battery alert
-----------------------------------------------------
-local lowBatteryTimer = hs.timer.doEvery(60, function()
-  local pct = hs.battery.percentage()
-  logger.i('battery level ' .. pct .. '%')
-  if pct <= 25 then
-    hs.alert.show('low battery ' .. pct .. '%')
-  end
-end)
+remap({ "cmd" }, "escape", press({ "cmd" }, "`"))
 ----------------------------------------------------
 -- caffeine
 ----------------------------------------------------
 local caffeine = hs.menubar.new()
 local function setCaffeineDisplay(state)
-    if state then
-        caffeine:setTitle('+')
-    else
-        caffeine:setTitle('-')
-    end
+	if state then
+		caffeine:setTitle("+")
+	else
+		caffeine:setTitle("-")
+	end
 end
 
 local function caffeineClicked()
-    setCaffeineDisplay(hs.caffeinate.toggle('displayIdle'))
+	setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
 end
 
 if caffeine then
-    caffeine:setClickCallback(caffeineClicked)
-    setCaffeineDisplay(hs.caffeinate.get('displayIdle'))
+	caffeine:setClickCallback(caffeineClicked)
+	setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
 end
-----------------------------------------------------
--- watcher
-----------------------------------------------------
-hs.caffeinate.watcher.new(function(_)
-  keyboardEventTap:start()
-  lowBatteryTimer:start()
-end):start()
