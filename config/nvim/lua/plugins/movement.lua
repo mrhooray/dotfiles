23dec02785286e79f0e715e7aede6a3f59a198ee
Rui Hu <code@mrhooray.com>
2025-09-15 22:55:20 -0700

    nvim - movement plugins

---
+++ config/nvim/lua/plugins/movement.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+  },
+  {
+    "tris203/precognition.nvim",
+    opts = {},
+  },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {},
+  },
+}
