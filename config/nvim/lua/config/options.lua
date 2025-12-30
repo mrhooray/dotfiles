c05e91ab14ee2fdf33de327fd22a61d47f614dfc
Rui Hu <code@mrhooray.com>
2025-12-29 16:54:06 -0800

    nvim - tabstop by filetype

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -11,6 +11,16 @@ vim.opt.tabstop = 4
 vim.opt.undofile = false
 vim.opt.writebackup = false
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "json" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
 if os.getenv("SSH_TTY") then
   vim.o.clipboard = "unnamedplus"
 
